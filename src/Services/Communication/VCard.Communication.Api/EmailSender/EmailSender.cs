d4be2256277dc040217e1138d31de9964936545b
Albert Szymański <alb.szym1@gmail.com>
2024-09-06 21:45:31 +0200

    feat: added basic communication service

---
+++ src/Services/Communication/VCard.Communication.Api/EmailSender/EmailSender.cs
@@ -0,0 +1,17 @@
+﻿namespace VCard.Communication.Api.EmailSender;
+
+internal sealed class EmailSender(EmailSenderConfiguration configuration, Logger<EmailSender> logger)
+{
+    public Task SendEmailAsync(string email, string subject, string message)
+    {
+        logger.LogInformation(
+            "Sending email to {Email} with subject {Subject} and message {Message} from {SenderName}",
+            email,
+            subject,
+            message,
+            configuration.SenderName
+        );
+
+        return Task.CompletedTask;
+    }
+}
---
+++ src/Services/Communication/VCard.Communication.Api/EmailSender/EmailSenderConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace VCard.Communication.Api.EmailSender;
+
+internal sealed class EmailSenderConfiguration
+{
+    public const string SectionName = "EmailSender";
+    public string SenderName { get; set; }
+    public string ApiKey { get; set; }
+}
